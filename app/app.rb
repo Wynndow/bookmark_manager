0408951433f88b3c30e9eb7901227788cf8830fb
Chris Wynne <chris.wynndow@gmail.com>
2016-01-13 18:48:25 +0000

    Shows multiple tags

--- app/app.rb
+++ app/app.rb
@@ -16,8 +16,9 @@ class BookmarkManager < Sinatra::Base
 
   post '/links' do
     link = Link.new(title: params[:title], url: params[:url])
-    tag = Tag.first(tag: params[:tag]) || Tag.create(tag: params[:tag])
-    link.tags << tag
+    params[:tag].split(', ').each do |tag|
+      link.tags << (Tag.first(tag: params[:tag]) || Tag.create(tag: tag))
+    end
     link.save
     redirect '/links'
   end
--- spec/feature/adding_tags_spec.rb
+++ spec/feature/adding_tags_spec.rb
@@ -5,4 +5,9 @@ feature 'Adding tags to links' do
     expect(page).to have_content('search')
   end
 
+  scenario 'Adding multiple tags to a link' do
+    submit_new_link(tag: 'search, find')
+    expect(Link.first.tags.map(&:tag)).to include('search', 'find')
+  end
+
 end
