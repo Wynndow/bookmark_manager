36bb46db1f75a3aab2a82d5b2d82845657e4d64a
Chris Wynne <chris.wynndow@gmail.com>
2016-01-14 17:36:59 +0000

    Updates recovery link address

--- app/model/send_recover_link.rb
+++ app/model/send_recover_link.rb
@@ -14,7 +14,7 @@ class SendRecoverLink
     mailer.send_message(ENV["MAILGUN_SANDBOX"], {from: "bookmarkmanager@mail.com",
                                                  to: user.email,
                                                  subject: "Reset your password",
-                                                 text: "Click here to reset your password https://cpwbookmarkmanager.herokuapp.com/reset_password?token=#{user.password_token}"})
+                                                 text: "Click here to reset your password https://cpwbookmarkmanager.herokuapp.com/users/reset_password?token=#{user.password_token}"})
   end
 
   private
