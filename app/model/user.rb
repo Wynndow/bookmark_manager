38dc7d9a70fec238c42c25f929e5a2d07895665c
Chris Wynne <chris.wynndow@gmail.com>
2016-01-14 10:37:47 +0000

    Adds validation of user model for username, email and password

--- app/model/user.rb
+++ app/model/user.rb
@@ -3,9 +3,9 @@ class User
   include DataMapper::Resource
 
   property :id,       Serial
-  property :username, String
-  property :email,    String
-  property :password, BCryptHash
+  property :username, String, required: true, unique: true
+  property :email,    String, required: true, unique: true, format: :email_address
+  property :password, BCryptHash, required: true
 
   attr_accessor :password_confirmation
   validates_confirmation_of :password,
--- spec/feature/adding_users_spec.rb
+++ spec/feature/adding_users_spec.rb
@@ -8,8 +8,12 @@ feature 'Adding new users' do
   end
 
   scenario 'A user is not created if password conf doesn\'t match' do
-    expect{ sign_up(password_conf: 'wrong') }.not_to change{User.count}
+    expect{ sign_up(password_conf: 'wrong') }.not_to change{ User.count }
     expect(page).to have_content('Password and confirmation do not match')
+  end
+
+  scenario 'A user can\'t sign up without entering an email address' do
+    expect{ sign_up(email: '') }.not_to change{ User.count }
   end
 
 end
