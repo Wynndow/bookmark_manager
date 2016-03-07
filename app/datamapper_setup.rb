b3427a060881cb5ff61976817679da9d05dd5dad
Chris Wynne <chris.wynndow@gmail.com>
2016-03-07 17:55:25 +0000

    Updates database path

--- app/datamapper_setup.rb
+++ app/datamapper_setup.rb
@@ -4,6 +4,6 @@ require_relative './model/link'
 require_relative './model/tag'
 require_relative './model/user'
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/lab_week_bookmark_manager_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
